7864dd0386042e6db7651a6f1b87f030f841f632
scout <scout.crooke@ssfs.org>
2019-10-02 10:10:30 -0400

    Finished unit 3 assignment option 3 by drawing a hexagon flower with turtle

--- assignment_option3.py
+++ assignment_option3.py
@@ -4,27 +4,51 @@ import turtle
 
 
 def get_side_length():
-    return float(input("What is the length of a side of the hexagon?"))
+    """
+    this function gets the length of a side of the hexagon from the user
+    :return: integer value of the side length
+    """
+    return int(input("What is the length of a side of the hexagon?"))
 
 
 def get_center_color():
+    """
+    this function gets the color of the center of the flower from the user
+    :return: string value of the color of the center of the flower
+    """
     return input("What is the color of the center of the flower?")
 
 
 def get_petal_color():
+    """
+    this function gets the color of the flower petals from the user
+    :return: string value of the color of the flower petals
+    """
     return input("What is the color of the flower petal?")
 
 
 def make_hexagon(color, length):
+    """
+    this function draws the center of the flower using the given color and length
+    :param color:
+    :param length:
+    :return: none
+    """
     turtle.color(color)
     turtle.begin_fill()
     for x in range(6):
         turtle.forward(length)
-        turtle.left(60)
+        turtle.right(60)
     turtle.end_fill()
 
 
 def make_petals(color, length):
+    """
+    this function draws the petal of the flower using the given color and length
+    :param color:
+    :param length:
+    :return: none
+    """
     turtle.color(color)
     turtle.begin_fill()
     for x in range(6):
@@ -36,8 +60,12 @@ def make_petals(color, length):
 def main():
     length = get_side_length()
     center = get_center_color()
-    get_petal_color()
+    petal = get_petal_color()
     make_hexagon(center, length)
+    for x in range(6):
+        turtle.fd(length)
+        turtle.right(60)
+        make_petals(petal, length)
 
 
 main()
