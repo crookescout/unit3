6e97bad5d22c1c4c30fc0a8a572cdc574899fb09
scout <scout.crooke@ssfs.org>
2019-10-17 10:47:30 -0400

    made corrections to the triangle function by creating a function with 3 parameters (a, b, c) and returned the answer

--- triangle.py
+++ triangle.py
@@ -2,15 +2,17 @@
 
 import math
 
+
+def area_of_triangle(a, b, c):
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area
+
+
 side_a = float(input("What is the length of side a?"))
 side_b = float(input("What is the length of side b?"))
 side_c = float(input("What is the length of side c?"))
 
+a = area_of_triangle(side_a, side_b, side_c)
 
-def area_of_triangle():
-    s = (side_a + side_b + side_c) / 2
-    a = math.sqrt(s * (s - side_a) * (s - side_b ) * (s - side_c))
-    print("The area of a triangle with side lengths", str(side_a) + ",", str(side_b) + ",", "and", str(side_c), "equals", a)
-
-
-area_of_triangle()
+print("The area of a triangle with side lengths", str(side_a) + ",", str(side_b) + ",", "and", str(side_c), "equals", a)
